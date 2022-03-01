06771bd87f9f72ae009946ba1bd410e9b26cea71
ilyshaav <55013715+ilyshaav@users.noreply.github.com>
2022-03-01 18:42:22 +0300

    добавление теста создания каталога

--- unit_test.py
+++ unit_test.py
@@ -1,4 +1,17 @@
+import os
 from file_operation import FileOperation
 class FilleTests(object):
     def __init__(self):
         pass
+    
+    def testCreateFile():
+        FileOperation.createDir('testCreateDir')
+        if os.path.exists('testCreateDir'):
+            print("createDir test pass ")
+            return True
+        else:
+            print("createDir test false ")
+            return False
+
+FilleTests.testCreateFile()
+    
