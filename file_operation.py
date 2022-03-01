2c41b8533c5284026e39990d2468f434a14caa8c
ilyshaav <55013715+ilyshaav@users.noreply.github.com>
2022-03-01 17:56:40 +0300

    создание классов

---
+++ file_operation.py
@@ -0,0 +1,3 @@
+class FileOperation:
+    def __init__(self):
+        pass
---
+++ unit_test.py
@@ -0,0 +1,4 @@
+from file_operation import FileOperation
+class FilleTests(object):
+    def __init__(self):
+        pass
